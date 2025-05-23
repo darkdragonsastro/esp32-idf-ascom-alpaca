82167f2036f604d07ced433968470b55d96bc082
Rick Bassham <brodrick.bassham@gmail.com>
2025-05-22 20:52:02 -0400

    feat: Add discovery header file

---
+++ src/alpaca_client/discovery.h
@@ -0,0 +1,18 @@
+#pragma once
+#ifndef __ALPACA_CLIENT_DISCOVERY_H__
+#define __ALPACA_CLIENT_DISCOVERY_H__
+
+#include <esp_err.h>
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+  esp_err_t alpaca_discover();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // __ALPACA_CLIENT_DISCOVERY_H__
